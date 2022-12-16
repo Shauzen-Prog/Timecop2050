99c885124bd08838bbd4235a0625ad8728a152b6
Shauzen-Prog <80788112+Shauzen-Prog@users.noreply.github.com>
2022-12-15 23:21:19 -0300

    Level 1 Rework

--- Assets/Scripts/Player/MVC/PlayerController.cs
+++ Assets/Scripts/Player/MVC/PlayerController.cs
@@ -8,19 +8,29 @@ public enum TypeOfController
     NoControls
 }
 
-public class PlayerController 
+public class PlayerController
 {
+    private Transform _playerTransform;
     private DragFingerMove _dragFingerMove;
 
     private Action _ArtificialUpdate;
 
     public PlayerController(Transform playerTransform)
     {
+        _playerTransform = playerTransform;
         _dragFingerMove = new DragFingerMove(playerTransform);
     }
 
     public void OnAwake()
     {
+    #if UNITY_EDITOR
+
+        _ArtificialUpdate = MoveWASDController;
+        
+        return;
+    #endif
+        
+        
         _ArtificialUpdate = _dragFingerMove.DragMove;
     }
     
@@ -51,4 +61,18 @@ public class PlayerController
     {
         Debug.Log("Touch To Go");
     }
+
+    #if UNITY_EDITOR
+        
+    private void MoveWASDController()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var _moveVector = new Vector3(horizontal, 0, vertical);
+        
+        _playerTransform.position += _moveVector * (20 * Time.deltaTime);
+    }
+        
+    #endif
 }
